a889115dd092690ae4dee247f537f446d3f4b884
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:49 +0700

    Commit: types.ts

---
+++ src/features/kandang/types.ts
@@ -0,0 +1,31 @@
+import { BaseResponseApi } from "@/interfaces/common";
+
+// Kandang types/interfaces
+export interface Kandang {
+  id: string;
+  namaKandang: string;
+  kapasitas: number;
+  lokasi: string;
+  petugasId: string;
+  petugasNama: string | null;
+  createdAt: string;
+  updateAt: string;
+}
+
+export interface CreateKandangDto {
+  namaKandang: string;
+  kapasitas: number;
+  lokasi: string;
+  petugasId: string;
+}
+
+export interface UpdateKandangDto {
+  namaKandang?: string;
+  kapasitas?: number;
+  lokasi?: string;
+  petugasId?: string;
+}
+
+export interface KandangFilters extends BaseResponseApi {
+  petugasId?: string;
+}
