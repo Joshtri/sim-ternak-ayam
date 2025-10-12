bdb8fd3e50864e1f5064425c47884df9ebf9c545
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:57 +0700

    Commit: show.tsx

---
+++ src/pages/users-management/show.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import UserManagementDetail from "@/features/users-management/detail";
+
+export default function UsersManagementShowPage() {
+  return <UserManagementDetail />;
+}
