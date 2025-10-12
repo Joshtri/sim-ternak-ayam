936625873ec0568868485f948d35e6362bf6e458
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:56 +0700

    Commit: show.tsx

---
+++ src/pages/jenis-kegiatan/show.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import JenisKegiatanDetail from "@/features/jenis-kegiatan/detail";
+export default function JenisKegiatanShowPage() {
+  return <JenisKegiatanDetail />;
+}
