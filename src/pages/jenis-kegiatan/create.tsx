f70cea05754b7b60a4602379f0800d7a73475f02
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:55 +0700

    Commit: create.tsx

---
+++ src/pages/jenis-kegiatan/create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import JenisKegiatanCreateForm from "@/features/jenis-kegiatan/create";
+
+export default function JenisKegiatanCreatePage() {
+  return <JenisKegiatanCreateForm />;
+}
