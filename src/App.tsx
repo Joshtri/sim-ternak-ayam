8672ff89385315320b225504809a84a0a4136a21
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:37 +0700

    Commit: App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import type { FunctionComponent } from "./common/types";
+import type { TanstackRouter } from "./main";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "@tanstack/react-router";
+import { ToastProvider } from "@heroui/toast";
+
+// import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
+
+const queryClient = new QueryClient();
+
+type AppProps = { router: TanstackRouter };
+
+const App = ({ router }: AppProps): FunctionComponent => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ToastProvider maxVisibleToasts={3000} placement="top-right" />
+
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
