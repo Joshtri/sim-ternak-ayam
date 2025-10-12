531db955e7ffddd5b519c3e2dd0ef2aa4adf6704
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:55 +0700

    Commit: list.tsx

---
+++ src/pages/jenis-kegiatan/list.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import JenisKegiatanList from "@/features/jenis-kegiatan/list";
+
+export default function JenisKegiatanListPage() {
+  return <JenisKegiatanList />;
+}
