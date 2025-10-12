4fef03ad6bed8e711b4de32ccab5ab5aeb57f711
Joshtri <stuffofyos1516@gmail.com>
2025-10-13 00:24:56 +0700

    Commit: create.tsx

---
+++ src/pages/mortalitas/create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import MortalitasCreateForm from "@/features/mortalitas/create";
+
+export default function MortalitasCreatePage() {
+  return <MortalitasCreateForm />;
+}
