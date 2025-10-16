d64478c156e88dfbf4287fb082254cc0023d116c
Joshtri <stuffofyos1516@gmail.com>
2025-10-17 04:04:39 +0700

    Commit: LaporanHeader.tsx

---
+++ src/features/laporan/operasional/components/LaporanHeader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function LaporanHeader() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Laporan Operasional</h1>
+      <p className="text-default-500">
+        Ringkasan kegiatan operasional berdasarkan periode
+      </p>
+    </div>
+  );
+}
