3965842ebcd340cf428e6a5fd6d3132fffa6f042
Joshtri <stuffofyos1516@gmail.com>
2025-10-17 04:04:39 +0700

    Commit: LaporanTabs.tsx

---
+++ src/features/laporan/operasional/components/LaporanTabs.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Card, CardBody, Tabs, Tab } from "@heroui/react";
+import { Activity, Building2, Users } from "lucide-react";
+
+import TabByJenis from "./TabByJenis";
+import TabByKandang from "./TabByKandang";
+import TabByPetugas from "./TabByPetugas";
+
+export default function LaporanTabs({ laporan }: any) {
+  return (
+    <Card>
+      <CardBody>
+        <Tabs fullWidth aria-label="Laporan Detail" variant="underlined">
+          <Tab
+            key="by-activity"
+            title={
+              <div className="flex items-center gap-2">
+                <Activity size={18} />
+                <span>Per Jenis Kegiatan</span>
+              </div>
+            }
+          >
+            <TabByJenis detailPerJenis={laporan.detailPerJenis} />
+          </Tab>
+
+          <Tab
+            key="by-kandang"
+            title={
+              <div className="flex items-center gap-2">
+                <Building2 size={18} />
+                <span>Per Kandang</span>
+              </div>
+            }
+          >
+            <TabByKandang detailPerKandang={laporan.detailPerKandang} />
+          </Tab>
+
+          <Tab
+            key="by-petugas"
+            title={
+              <div className="flex items-center gap-2">
+                <Users size={18} />
+                <span>Per Petugas</span>
+              </div>
+            }
+          >
+            <TabByPetugas detailPerPetugas={laporan.detailPerPetugas} />
+          </Tab>
+        </Tabs>
+      </CardBody>
+    </Card>
+  );
+}
