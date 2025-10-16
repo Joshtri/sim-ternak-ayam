6a4253f3510dd388a86261031cfaa2f2a662927f
Joshtri <stuffofyos1516@gmail.com>
2025-10-16 12:28:53 +0700

    Commit: edit.tsx

--- src/pages/panen/edit.tsx
+++ src/pages/panen/edit.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import { PanenEditForm } from "@/features/panen/edit";
 
-export default function edit() {
-  return (
-    <div>edit</div>
-  )
+export default function PanenEditPage() {
+  return <PanenEditForm />;
 }
