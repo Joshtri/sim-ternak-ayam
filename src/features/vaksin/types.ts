6228d3799f248ce67ed9543f3477048e7a934ed6
Joshtri <stuffofyos1516@gmail.com>
2025-10-16 12:28:53 +0700

    Commit: types.ts

--- src/features/vaksin/types.ts
+++ src/features/vaksin/types.ts
@@ -3,16 +3,22 @@ import { BaseEntity } from "@/interfaces/common";
 export interface Vaksin extends BaseEntity {
   namaVaksin: string;
   stok: number;
+  bulan: number;
+  tahun: number;
 }
 
 export interface CreateVaksinDto {
   namaVaksin: string;
   stok: number;
+  bulan: number;
+  tahun: number;
 }
 
 export interface UpdateVaksinDto extends Pick<BaseEntity, "id"> {
   namaVaksin?: string;
   stok?: number;
+  bulan?: number;
+  tahun?: number;
 }
 
 export interface UpdateStockDto {
