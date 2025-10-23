0bf07bc1121c5c295a8757a3c82504723316fd20
Joshtri <stuffofyos1516@gmail.com>
2025-10-23 07:50:00 +0700

    Commit: types.ts

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -3,3 +3,22 @@ export interface LoginDto {
     password: string;
 }
 
+export interface KandangManaged {
+    id: string;
+    namaKandang: string;
+    kapasitas: number;
+    lokasi: string;
+}
+
+export interface CurrentUser {
+    id?: string;
+    username?: string;
+    email?: string;
+    fullName?: string;
+    noWA?: string;
+    role?: string;
+    kandangsManaged?: KandangManaged[];
+    createdAt?: string;
+    updateAt?: string;
+}
+
