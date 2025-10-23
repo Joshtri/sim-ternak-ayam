53a5e3f14d1fd2d5920a7783be628dcb561ecc51
Joshtri <stuffofyos1516@gmail.com>
2025-10-23 07:50:00 +0700

    Commit: biayaService.ts

--- src/features/biaya/services/biayaService.ts
+++ src/features/biaya/services/biayaService.ts
@@ -1,4 +1,4 @@
-import { CreateBiayaDto, Biaya, UpdateBiayaDto } from "../types";
+import { CreateBiayaDto, Biaya, UpdateBiayaDto, BiayaBulananResponseDto } from "../types";
 
 import api, { ApiResponse, PaginatedResponse } from "@/lib/axios";
 
@@ -11,6 +11,15 @@ export const biayaService = {
 
   getBiayaById: async (id: string): Promise<Biaya> => {
     const response = await api.get<ApiResponse<Biaya>>(`/biayas/${id}`);
+
+    return response.data.data;
+  },
+
+  // NEW: Get monthly recap of costs
+  getRekapBulanan: async (bulan: number, tahun: number): Promise<BiayaBulananResponseDto> => {
+    const response = await api.get<ApiResponse<BiayaBulananResponseDto>>(
+      `/biayas/rekap-bulanan/${bulan}/${tahun}`
+    );
 
     return response.data.data;
   },
