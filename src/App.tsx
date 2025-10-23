260ccf25ad73223a9054831ffea43412cac68dbe
Joshtri <stuffofyos1516@gmail.com>
2025-10-23 07:49:59 +0700

    Commit: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import type { FunctionComponent } from "./common/types";
 import type { TanstackRouter } from "./main";
 
+import { HeroUIProvider } from "@heroui/react";
+import { ToastProvider } from "@heroui/toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "@tanstack/react-router";
-import { ToastProvider } from "@heroui/toast";
-import { HeroUIProvider } from "@heroui/react";
 
 import { ThemeProvider } from "./contexts/ThemeContext";
-import InteractiveCursor from "./components/Cursor/InteractiveCursor";
 
 // import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
 
